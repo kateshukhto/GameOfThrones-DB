91ae8675c701c4824614ad511a586b0bc63ab9ea
kateshukhto <ekaterinasuhto@gmail.com>
2021-07-08 13:17:38 +0300

    Updates

---
+++ precache-manifest.d47b866edff8b9bc7e32190dffe5ff21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b9fafa20a7ddcb7310",
+    "url": "/GameOfThtones-DB/static/css/main.ba26d374.chunk.css"
+  },
+  {
+    "revision": "a3b9fafa20a7ddcb7310",
+    "url": "/GameOfThtones-DB/static/js/main.a3b9fafa.chunk.js"
+  },
+  {
+    "revision": "101eae718cde01ed125b",
+    "url": "/GameOfThtones-DB/static/css/1.97c871f2.chunk.css"
+  },
+  {
+    "revision": "101eae718cde01ed125b",
+    "url": "/GameOfThtones-DB/static/js/1.101eae71.chunk.js"
+  },
+  {
+    "revision": "124076d20c8eb23545df",
+    "url": "/GameOfThtones-DB/static/js/runtime~main.124076d2.js"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/GameOfThtones-DB/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "4c08c8ba7d9c6844d60155cf6c505004",
+    "url": "/GameOfThtones-DB/index.html"
+  }
+];
